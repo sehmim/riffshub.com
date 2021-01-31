b369c7e6120ea7792df1ca9e8f5da973cc9184bb
sehmim2 <sehmim2010@hotmail.com>
2021-01-30 22:55:14 -0500

    refactoring card to own file

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+import { IonButton, IonLabel, IonItem, IonCard } from '@ionic/react';
+import DEFAULT_PROFILE from "../assets/fin.jpg"
+
+const PostCard = ({ item }) => {
+
+    const videoElm = useRef();
+    const [videoUrl, setVideoUrl] = useState("")
+    const [isPlaying, setisPlaying] = useState(false)
+
+    // const { state, dispatch } = useContext(AppContext)
+    
+    const pauseVideo = () => {
+      if (!isPlaying) {
+        videoElm.current.play()
+        setisPlaying(true)
+      } else {
+        videoElm.current.pause()
+        setisPlaying(false)
+      }
+    }
+    
+    useEffect(()=> {
+      fetchVideos()
+    },[])
+
+    const fetchVideos = async () => {
+      try {
+        const vid = await Storage.get(item.vidUrl, { level : 'public', expires: 1500 })
+        setVideoUrl(vid)
+
+      } catch (error) {
+        console.log("Error: ", error)
+      }
+    }
+    
+
+    return (<IonCard>
+      <IonItem>
+        <IonLabel className="username">@{item.owner}</IonLabel>
+        <IonButton color="light" slot="end">
+          <a href="/">
+            <img className="profile-pic-small" src={DEFAULT_PROFILE}></img>
+          </a>
+        </IonButton>
+      </IonItem>
+      <IonItem onClick={pauseVideo}>
+        <div className="video-viewer-body">
+        <video 
+          ref={videoElm}
+          className="video-viewer"
+          src={videoUrl} 
+          />
+        </div>
+      </IonItem>
+    </IonCard>)}
+
+export default PostCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
-import { IonButton, IonLabel, IonPage , IonContent, IonItem, IonCard } from '@ionic/react';
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import { IonPage , IonContent, IonItem, IonCard } from '@ionic/react';
+import React, { useState, useEffect, useContext } from 'react';
 
 // Styles and assets
 import './Home.css';
 import "../App.css"
-import DEFAULT_PROFILE from "../assets/fin.jpg"
 
 // Modules
-import { Storage, API } from 'aws-amplify';
+import { API } from 'aws-amplify';
 import { listPosts } from '../graphql/queries'
 import { getCurrentUser } from "../Util";
 
@@ -16,6 +15,7 @@ import { AppContext } from "../State"
 import SigninToPostButton from "../components/SigninToPostButton";
 import PostContentButton from "../components/PostContentButton"
 import ProfileHeader from "../components/ProfileHeader"
+import PostCard from "../components/PostCard"
 
 const Home = () => {
   const [posts, setPosts] = useState([])
@@ -57,7 +57,7 @@ const Home = () => {
           {
             posts.map((item, index)=> {
               return(
-                <Card item={item} key={index}/>
+                <PostCard item={item} key={index}/>
               )
             })
           }
@@ -67,59 +67,6 @@ const Home = () => {
 };
 
 
-
-const Card = ({ item }) => {
-
-    const videoElm = useRef();
-    const [videoUrl, setVideoUrl] = useState("")
-    const [isPlaying, setisPlaying] = useState(false)
-
-    // const { state, dispatch } = useContext(AppContext)
-    
-    const pauseVideo = () => {
-      if (!isPlaying) {
-        videoElm.current.play()
-        setisPlaying(true)
-      } else {
-        videoElm.current.pause()
-        setisPlaying(false)
-      }
-    }
-    
-    useEffect(()=> {
-      fetchVideos()
-    },[])
-
-    const fetchVideos = async () => {
-      try {
-        const vid = await Storage.get(item.vidUrl, { level : 'public', expires: 1500 })
-        setVideoUrl(vid)
-
-      } catch (error) {
-        console.log("Error: ", error)
-      }
-    }
-    
-
-    return (<IonCard>
-      <IonItem>
-        <IonLabel className="username">@{item.owner}</IonLabel>
-        <IonButton color="light" slot="end">
-          <a href="/">
-            <img className="profile-pic-small" src={DEFAULT_PROFILE}></img>
-          </a>
-        </IonButton>
-      </IonItem>
-      <IonItem onClick={pauseVideo}>
-        <div className="video-viewer-body">
-        <video 
-          ref={videoElm}
-          className="video-viewer"
-          src={videoUrl} 
-          />
-        </div>
-      </IonItem>
-    </IonCard>)}
 
 export { Home };
 
